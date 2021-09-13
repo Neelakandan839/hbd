a825bef0d4d470f00351bba2563dc5cf0699d5ff
Neelakandan <86792263+Neelakandan839@users.noreply.github.com>
2021-09-13 22:18:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Eman Sabir" && b=="27-06-1996")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
